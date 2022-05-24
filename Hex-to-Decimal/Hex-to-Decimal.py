59f6dc70ce29f9dcd01b12597cc66fd7ef4d7926
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-05-24 11:23:24 +0000

    Added Denary to Hex option

--- Hex-to-Decimal/Hex-to-Decimal.py
+++ Hex-to-Decimal/Hex-to-Decimal.py
@@ -1,7 +1,6 @@
-hex = input("Enter a hex digit: ")
+import os
 
-def hex_to_denary(hex_number):
-    dictionary = {
+dictionary_hex_to_decimal = {
         "1":1,
         "2":2,
         "3":3,
@@ -19,10 +18,58 @@ def hex_to_denary(hex_number):
         "f":15
     }
 
-    digit1 = hex_number[0]
-    digit2 = hex_number[1].lower()
-    denary_digit1 = int(dictionary[f"{digit1}"]) * 16
-    denary_digit2 = int(dictionary[f"{digit2}"])
-    print(denary_digit1 + denary_digit2)
+dictionary_denary_to_hex = {
+        1:"1",
+        2:"2",
+        3:"3",
+        4:"4",
+        5:"5",
+        6:"6",
+        7:"7",
+        8:"8",
+        9:"9",
+        10:"a",
+        11:"b",
+        12:"c",
+        13:"d",
+        14:"e",
+        15:"f"
+    }
 
-hex_to_denary(hex)
+print(dictionary_denary_to_hex[10])
+
+def denary_to_hex(denary_number):
+    first_character = dictionary_denary_to_hex[int(denary_number) // 16]
+    second_character = dictionary_denary_to_hex[f"{int(denary_number) - (int(first_character) * 16)}"]
+    print(first_character + second_character + "\n")
+
+def hex_to_denary(hex_number):
+    digit1, digit2 = hex_number[0].lower(), hex_number[1].lower()
+    denary_digit1, denary_digit2 = (int(dictionary_hex_to_decimal[digit1]) * 16), (int(dictionary_hex_to_decimal[digit2]))
+    print(str(denary_digit1) + str(denary_digit2) + "\n")
+
+### Main Menu ###
+while True:
+    os.system("clear")
+    choice = input("""Choose an option:
+
+[1] Hex → Denary
+[2] Denary → Hex
+[exit] Quit the program
+: """)
+    if choice == '1':
+        hex_number_inputted = input("Enter a hex number\n> ")
+        hex_to_denary(hex_number_inputted)
+        temp = input("Press ENTER to continue ")
+        continue
+    elif choice == '2':
+        denary_number_inputted = input("Enter a denary number to convert to hex, between 1 and 255\n> ")
+        denary_to_hex(denary_number_inputted)
+        temp = input("Press ENTER to continue ")
+        continue
+    elif choice == 'exit':
+        print("Quitting...")
+        exit(0)
+    else:
+        print("Incorrect option")
+        continue

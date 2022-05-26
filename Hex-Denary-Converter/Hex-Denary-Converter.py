cf2a0050b6d6cfa248069893c93776574f49da50
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-05-26 10:35:48 +0100

    Fixed denary to hex and capitalised output

--- Hex-Denary-Converter/Hex-Denary-Converter.py
+++ Hex-Denary-Converter/Hex-Denary-Converter.py
@@ -28,12 +28,12 @@ dictionary_denary_to_hex = {
         7:"7",
         8:"8",
         9:"9",
-        10:"a",
-        11:"b",
-        12:"c",
-        13:"d",
-        14:"e",
-        15:"f"
+        10:"A",
+        11:"B",
+        12:"C",
+        13:"D",
+        14:"E",
+        15:"F"
     }
 
 
@@ -41,16 +41,19 @@ def denary_to_hex(denary_number):
     if (int(denary_number) > 255) or (int(denary_number) < 1):
         print("Number too large or too small, try again")
         return None
-    first_character = dictionary_denary_to_hex[int(denary_number)/16 if int(denary_number) % 16 else int(denary_number) // 16]
-    second_character = dictionary_denary_to_hex[int(denary_number) - (int(first_character) * 16)]
-    print(first_character + second_character + "\n")
+    if int(denary_number) <= 16:
+        first_character = dictionary_denary_to_hex[f"{denary_number.lower()}"]
+    else:
+        first_character = dictionary_denary_to_hex[int(denary_number) // 16]
+        second_character = dictionary_denary_to_hex[int(denary_number) - (int(first_character) * 16)]
+        print(first_character + second_character + "\n")
 
 def hex_to_denary(hex_number):
     if len(hex_number) == 2:
         digit1, digit2 = hex_number[0].lower(), hex_number[1].lower()
         # Must use formatted string when accessing dictionary because it has to be a string that is looked-up
         denary_digit1, denary_digit2 = dictionary_hex_to_decimal[f"{digit1}"], dictionary_hex_to_decimal[f"{digit2}"]
-        print((denary_digit1 * 16) + denary_digit2)
+        print((denary_digit1 * 16) + denary_digit2.upper())
     elif len(hex_number) == 1:
         digit1 = hex_number[0].lower()
         denary_digit1 = int(dictionary_hex_to_decimal[f"{digit1}"])

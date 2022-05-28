ffe3acce6dc79c92510d8995e00a2298e1a332f7
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-05-28 20:21:32 +0000

    Fixed progress bar logic

--- Progress-Bar/ProgressBar.py
+++ Progress-Bar/ProgressBar.py
@@ -2,15 +2,15 @@ import time
 
 def progress_bar(completed_progress, total_progress):
     symbol = "█"
-    percentage = completed_progress/total_progress*100
+    percentage = int(completed_progress/total_progress*100)
     percentage = str(percentage)
-    print(type(percentage))
     to_print_amount = completed_progress
     to_print_dashes = total_progress - to_print_amount
     return "|" + symbol * to_print_amount + "-" * to_print_dashes + "|" + percentage + "%"
 
 
-for x in range(100):
+for x in range(1, 101):
     data = progress_bar(x, 100)
-    print(data, end='\x1b[1K\r', flush=True)
+    print(data, end='\r', flush=True)
     time.sleep(0.05)
+print("\n")

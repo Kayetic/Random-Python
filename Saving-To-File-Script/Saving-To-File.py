6fc015b6563395eec690f43b37ef3ee763ae30f8
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-06-06 08:37:16 +0000

    Added docstrings

--- Saving-To-File-Script/Saving-To-File.py
+++ Saving-To-File-Script/Saving-To-File.py
@@ -1,14 +1,20 @@
-# function to append a line to the end of a file called "data.txt"
-
 def append_to_file(filename, line):
+    """"
+    Appending a line to the end of a file called "data.txt"
+    Parameters:filename (string) - the name of the file to be read
+    line (string) - the line to be appended to the file
+    """
     with open(filename, "a+") as file:
         file.write(line + "\n")
         file.close()
 
 
-# function to read the contents of a file called "data.txt"
 def read_file(filename):
-    with open(filename, "r") as file:
+    """
+    Opening a file and reading the contents into a variable called 'lines'
+    Parameters:filename (string) - the name of the file to be read
+    """
+    with open(filename, "r", encoding="utf-8") as file:
         lines = file.readlines()
         file.close()
     return lines
@@ -17,5 +23,3 @@ name = input("Enter your name: ")
 age = input("Enter your age: ")
 city = input("Enter your city: ")
 sex = input("E")
-
-data_to_add = 

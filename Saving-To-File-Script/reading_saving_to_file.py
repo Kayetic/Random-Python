b04f2d6ddec23867f061faf1077361b756806dba
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-06-06 08:51:50 +0000

    Implemented checking for empty file

--- Saving-To-File-Script/reading_saving_to_file.py
+++ Saving-To-File-Script/reading_saving_to_file.py
@@ -1,12 +1,26 @@
 def append_to_file(filename, line):
-    """"
-    Appending a line to the end of a file called "data.txt"
-    Parameters:filename (string) - the name of the file to be read
+    """
+    Appending a line to the end of a file called 'data.txt'
+    Parameters: filename (string) - the name of the file to be read
     line (string) - the line to be appended to the file
     """
-    with open(filename, "a+") as file:
-        file.write(line + "\n")
-        file.close()
+
+    def is_file_empty(filename):
+        """ Check if file is empty by reading first character in it"""
+        with open(filename, 'r', encoding='utf-8') as read_obj:
+            # Read first character
+            first_character = read_obj.read(1)
+            # If not fetched, then file is empty
+            if not first_character:
+                return True
+        return False
+
+    with open(filename, 'a+', encoding='utf-8') as file:
+        if is_file_empty(filename) is True:
+            file.write(line)
+        else:
+            file.write("\n" + line)
+            file.close()
 
 
 def read_file(filename):
@@ -14,7 +28,7 @@ def read_file(filename):
     Opening a file and reading the contents into a variable called 'lines'
     Parameters:filename (string) - the name of the file to be read
     """
-    with open(filename, "r", encoding="utf-8") as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         lines = file.readlines()
         file.close()
     return lines
@@ -22,4 +36,8 @@ def read_file(filename):
 name = input("Enter your name: ")
 age = input("Enter your age: ")
 city = input("Enter your city: ")
-sex = input("E")
+sex = input("Enter your sex (M/F): ")
+
+### Testing the functions ###
+data_to_append = f'{name}, {age}, {city}, {sex}'
+append_to_file("data.txt", data_to_append)

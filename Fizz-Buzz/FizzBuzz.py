544b31f575e46330d5df77a822f1eda88ca3a246
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-06-10 18:33:06 +0000

    Converted to function

--- Fizz-Buzz/FizzBuzz.py
+++ Fizz-Buzz/FizzBuzz.py
@@ -1,13 +1,17 @@
 number = int(input("Enter a number: "))
-for i in range(1, number):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-        continue
-    elif i % 3 == 0:
-        print("Fizz")
-        continue
-    elif i % 5 ==0:
-        print("Buzz")
-        continue
-    else:
-      print(i)
+
+def fizzbuzz(number):
+    for i in range(1, number):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+            continue
+        elif i % 3 == 0:
+            print("Fizz")
+            continue
+        elif i % 5 ==0:
+            print("Buzz")
+            continue
+        else:
+            print(i)
+
+fizzbuzz(number)

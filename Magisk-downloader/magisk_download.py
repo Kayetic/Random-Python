765ba329a55ebf0cfaf542db2a8fde8f15672575
Kayetic <bartek.bak@protonmail.com>
2022-06-20 18:04:14 +0100

    Working download of one file

--- Magisk-downloader/magisk_download.py
+++ Magisk-downloader/magisk_download.py
@@ -1,10 +1,18 @@
-from textwrap import indent
 import requests
 import json
+import shutil
 
 response = requests.get('https://api.github.com/repos/topjohnwu/Magisk/releases/latest')
-# print(response.json())
-
-print(type(response.json()))
+# print(type(response.json()))
 data = response.json()
-print(data[0]['url'])
+download_url = data["assets"][0]["browser_download_url"]
+
+github_response = requests.get(download_url)
+text = download_url.split("/")
+filename = text[-1]
+print(filename)
+    
+open(filename, "wb").write(github_response.content)
+print("Downloaded")
+print("\n")
+

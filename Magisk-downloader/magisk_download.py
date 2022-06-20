b2880ddd71bd19fcf233a5957f858fae5da7bbf9
Kayetic <bartek.bak@protonmail.com>
2022-06-20 18:44:06 +0100

    Cleaned some more code

--- Magisk-downloader/magisk_download.py
+++ Magisk-downloader/magisk_download.py
@@ -3,17 +3,19 @@ import json
 import shutil
 
 response = requests.get('https://api.github.com/repos/topjohnwu/Magisk/releases/latest')
-# print(type(response.json()))
 data = response.json()
 download_url = data["assets"][0]["browser_download_url"]
 
 github_response = requests.get(download_url)
 text = download_url.split("/")
 filename = text[-1]
-print(filename)
+print(f"Downloading: {filename}")
     
-# open(filename, "wb").write(github_response.content)
+open(filename, "wb").write(github_response.content)
 print("Downloaded")
 print("\n")
 
 shutil.copyfile(filename, f"./{filename[:-4]}.zip")
+
+temp = input("\nPress ENTER to exit")
+exit()

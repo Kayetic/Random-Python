5d81352c6fc8de47e9d8f9bf8100df2ea336fe94
Kayetic <bartek.bak@protonmail.com>
2022-07-06 19:30:08 +0100

    Started adding LSPosed

--- Magisk-downloader/magisk_download.py
+++ Magisk-downloader/magisk_download.py
@@ -2,9 +2,18 @@ import requests
 import json
 import shutil
 
-response = requests.get('https://api.github.com/repos/topjohnwu/Magisk/releases/latest')
+while True:
+    choice = input("""Choose what you would like to download
+[1] Magisk (Manager + Recovery Flashable Zip)
+[2] LSPosed (Zygisk)
+[3] Universal auth files""")
+
+# response = requests.get('https://api.github.com/repos/topjohnwu/Magisk/releases/latest')
+response = requests.get('https://api.github.com/repos/LSPosed/LSPosed/releases/latest')
 data = response.json()
-download_url = data["assets"][0]["browser_download_url"]
+download_url = data["assets"][1]["browser_download_url"]
+print(download_url)
+
 
 github_response = requests.get(download_url)
 text = download_url.split("/")

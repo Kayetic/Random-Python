6cabacfa94ef5a37e82d4ad353f3dd42e4828c17
Kayetic <bartek.bak@protonmail.com>
2022-07-06 20:00:29 +0100

    LSPosed downloads finished

--- Magisk-downloader/magisk_download.py
+++ Magisk-downloader/magisk_download.py
@@ -1,30 +1,43 @@
 import requests
 import json
 import shutil
+import platform
+import os
 
-while True:
-    choice = input("""Choose what you would like to download
+os.system("cls") if 'Windows' in platform.system() else os.system("clear")
+choice = input("""Choose what you would like to download
 [1] Magisk (Manager + Recovery Flashable Zip)
 [2] LSPosed (Zygisk)
-[3] Universal auth files""")
+[3] Universal auth files
+[exit] Exit
+>>> """)
+if choice == "1":
+    response = requests.get('https://api.github.com/repos/topjohnwu/Magisk/releases/latest')
+    data = response.json()
+    download_url = data["assets"][0]["browser_download_url"]
+    github_response = requests.get(download_url)
+    text = download_url.split("/")
+    filename = text[-1]
+    print(f"Downloading: {filename}")
+        
+    open(filename, "wb").write(github_response.content)
+    print("\nDownloaded Magisk files")
+    print("\n")
 
-# response = requests.get('https://api.github.com/repos/topjohnwu/Magisk/releases/latest')
-response = requests.get('https://api.github.com/repos/LSPosed/LSPosed/releases/latest')
-data = response.json()
-download_url = data["assets"][1]["browser_download_url"]
-print(download_url)
+    shutil.copyfile(filename, f"./{filename[:-4]}.zip")
 
+    temp = input("\nPress ENTER to exit\n")
+    exit()
+elif choice == "2":
+    response = requests.get('https://api.github.com/repos/LSPosed/LSPosed/releases/latest')
+    data = response.json()
+    download_url = data["assets"][1]["browser_download_url"]
+    github_response = requests.get(download_url)
+    text = download_url.split("/")
+    filename = text[-1]
+    print(f"Downloading: {filename}")
 
-github_response = requests.get(download_url)
-text = download_url.split("/")
-filename = text[-1]
-print(f"Downloading: {filename}")
-    
-open(filename, "wb").write(github_response.content)
-print("Downloaded")
-print("\n")
-
-shutil.copyfile(filename, f"./{filename[:-4]}.zip")
-
-temp = input("\nPress ENTER to exit")
-exit()
+    open(filename, "wb").write(github_response.content)
+    print("Downloaded LSPosed")
+    temp = input("\nPress ENTER to exit\n")
+    exit()

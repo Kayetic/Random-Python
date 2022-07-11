390d3c9ff2b11d2bd9074d04aec68f940794c8f4
Kayetic <bartek.bak@protonmail.com>
2022-07-11 10:49:10 +0100

    Initialised taskkill

---
+++ Taskkill/main.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+from tkinter import filedialog as fd
+import psutil
+import time
+import signal
+
+root = tk.Tk()
+root.withdraw()
+
+def handler(signum, frame):
+    print('What are you, mentally weak?')
+
+signal.signal(signal.SIGINT, handler)
+
+temp = open("Taskkill/blacklist.txt")
+programs = temp.read().splitlines()
+
+print(programs)
+
+choice = input("Do you want to blacklist a program? (y/n) ")
+if choice == "y":
+    program_path = fd.askopenfilename(filetypes=[("Program", "*.exe")])
+    program_name = program_path.split("/")[-1]
+    programs.append(program_name)
+    temp = open("blacklist.txt", "w")
+    for program in programs:
+        temp.write(program + "\n")
+    temp.close()
+    print("Program added to blacklist.")
+
+while True:
+    for program in psutil.process_iter():
+        if program.name() in programs:
+            program.kill()
+            print(f"Killing {program.name()}")
+    time.sleep(1)

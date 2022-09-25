82950dfdfcaddcf457006b2802ed21beba8f9e71
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-09-25 19:14:04 +0000

    tried fixing status code concatenatio

--- Name-Sniper/name-sniper.py
+++ Name-Sniper/name-sniper.py
@@ -6,45 +6,43 @@ import string
 available_names = []
 
 def check_username(username):
-    r = requests.get("https://api.mojang.com/users/profiles/minecraft/" + username)
-    if r.status_code == 204:
+    request_data = requests.get("https://api.mojang.com/users/profiles/minecraft/" + username)
+    if request_data.status_code == 204:
         print("Username " + username + " is available")
         available_names.append(username)
-    elif r.status_code == 200:
+    elif request_data.status_code == 200:
         print("Username " + username + " is taken")
-    else:
-        print("Error: " + r.status_code)
 
 #check if the username is available
-# check_username("cat239746")
+check_username("cat")
 
 #define a function to generate a string of random 3 letters
 
 
 
-def generate_username():
-    username = letter_random_string()
-    try:
-        check_username(username)
-    except TypeError:
-        print("Error: " + r.status_code)
-        pass
+# def generate_username():
+#     username = letter_random_string()
+#     try:
+#         check_username(username)
+#     except TypeError:
+#         print("Error: " + r.status_code)
+#         pass
 
-def letter_random_string():
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(3))
+# def letter_random_string():
+#     letters = string.ascii_lowercase
+#     return ''.join(random.choice(letters) for i in range(3))
 
 
-for i in range(999):
-    generate_username()
+# for i in range(999):
+#     generate_username() + i
 
-print(available_names)
+# print(available_names)
 
-def write_to_file():
-    with open("available_names.txt", "w") as f:
-        for name in available_names:
-            f.write(name + "\n")
+# def write_to_file():
+#     with open("available_names.txt", "w") as f:
+#         for name in available_names:
+#             f.write(name + "\n")
 
-write_to_file()
+# write_to_file()
 
 print("Done")

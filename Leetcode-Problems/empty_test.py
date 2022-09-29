6eaf9997e0175d068750afcb81ba29f01c49d5f1
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-09-29 09:39:23 +0000

    added all questions and main loops for

--- Leetcode-Problems/empty_test.py
+++ Leetcode-Problems/empty_test.py
@@ -1,12 +1,24 @@
 # pseudocode:
 
 def quiz_game():
-    #score = 0
-    #for i in range(10):
-        #display question and possible answers
-        #get user input
-        #if user input is correct:
-            #score += 1
-        #if user input is incorrect:
-            #score += 0
-        
+    questions = ["What is the capital of France?", "How many legs does a spider have?", "What is the current US president?", "What is the capital of the US?", "What was Meta Platforms Inc formerly known as?"]
+    possible_answers = [["Beijing", "Paris", "London", "New York"], ["2", "4", "6", "8"], ["Joe Biden", "Donald Trump", "Mr Faulkner", "Dr. Sins"], ["Washington DC", "Dubai", "Istanbul", "Hong Kong"], ["Facebook", "Google", "Amazon", "Twitter"]]
+    correct_answers = ["Paris", "8", "Joe Biden", "Washington DC", "Facebook"]
+    
+    score = 0
+    for i in range(len(questions)):
+        print(questions[i])
+        print("Possible answers:")
+        for j in range(len(possible_answers)):
+            print(possible_answers[i][j])
+        user_answer = input("Enter the answer: ")
+        if user_answer == correct_answers[i]:
+            print("Correct!")
+            score += 1
+        else:
+            print("Incorrect")
+            continue
+    return score
+
+print("Score:")
+print(quiz_game())

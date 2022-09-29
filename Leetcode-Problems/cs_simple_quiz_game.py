fddd59543385295b49a0f3533bdd6bfd51a19192
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2022-09-29 12:47:54 +0000

    cleaned up function

--- Leetcode-Problems/cs_simple_quiz_game.py
+++ Leetcode-Problems/cs_simple_quiz_game.py
@@ -1,11 +1,13 @@
 import time
 import os
 
-def quiz_game():
-    questions = ["What is the capital of France?", "How many legs does a spider have?", "What is the current US president?", "What is the capital of the US?", "What was Meta Platforms Inc formerly known as?"]
-    possible_answers = [["Beijing", "Paris", "London", "New York"], ["2", "4", "6", "8"], ["Joe Biden", "Donald Trump", "Mr Faulkner", "Dr. Sins"], ["Washington DC", "Dubai", "Istanbul", "Hong Kong"], ["Facebook", "Google", "Amazon", "Twitter"]]
-    correct_answers = ["Paris", "8", "Joe Biden", "Washington DC", "Facebook"]
-    
+questions_to_ask = ["What is the capital of France?", "How many legs does a spider have?", "What is the current US president?", "What is the capital of the US?", "What was Meta Platforms Inc formerly known as?"]
+
+possible_answers = [["Beijing", "Paris", "London", "New York"], ["2", "4", "6", "8"], ["Joe Biden", "Donald Trump", "Mr Faulkner", "Dr. Sins"], ["Washington DC", "Dubai", "Istanbul", "Hong Kong"], ["Facebook", "Google", "Amazon", "Twitter"]]
+
+correct_answers = ["Paris", "8", "Joe Biden", "Washington DC", "Facebook"]
+
+def quiz_game(questions, possible_answers, correct_answers):
     score = 0
     for i in range(len(questions)):
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -16,12 +18,12 @@ def quiz_game():
         user_answer = input("\nEnter the answer: ")
         if user_answer == correct_answers[i]:
             print("Correct!")
-            time.sleep(0.75)
+            time.sleep(0.5)
             score += 1
         else:
             print("Incorrect")
             continue
     return score
 
-final_score = quiz_game()
+final_score = quiz_game(questions_to_ask, possible_answers, correct_answers)
 print("Score: " + str(final_score))

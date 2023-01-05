7d1b0acc9328ce00ddfead0050aafbbc22f006db
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2023-01-05 10:10:18 +0000

    reverse the number

--- Leetcode-Problems/9 - Palindrome Number.py
+++ Leetcode-Problems/9 - Palindrome Number.py
@@ -1,13 +1,6 @@
 def isPalindrome(x):
-    x = str(x)
-    for i in range(len(x)):
-        # print(i)
-        # if x[i] == x[-i]:
-        #     continue
-        # else:
-        #     return False
-        print(x[-i])
-    return True
+    # reversing number
+    print(x[])
 
 
 print(isPalindrome(121))
